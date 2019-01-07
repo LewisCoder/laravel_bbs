3708f85a67cf2daac9afbb10f4afaa295d5c90db
Lewis <zlwcoder@126.com>
2019-01-07 03:00:44 +0000

    显示个人用户中心页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function show(User $user)
+    {
+    	return view('users.show',compact('user'));
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
